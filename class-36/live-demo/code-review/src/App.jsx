e5370216632a5f28cc364a688aa1a393376ea1ba
Ryan Gallaway <rkgallaway@msn.com>
2023-07-07 12:16:01 -0700

    class assets added

---
+++ class-36/live-demo/code-review/src/App.jsx
@@ -0,0 +1,18 @@
+import Categories from './Components/Categories'
+import Footer from './Components/Footer'
+import Header from './Components/Header'
+import Products from './Components/Products'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Categories />
+      <Products />
+      <Footer />
+    </>
+  )
+}
+
+export default App
